5027f9392020dc3ac36668e9609c8083619e7157
maybeElse <93097725+maybeElse@users.noreply.github.com>
2022-10-24 16:06:15 -0400

    simplified payload construction

--- mbtaReader.py
+++ mbtaReader.py
@@ -5,7 +5,7 @@ from functools import wraps
 import mbtaObjects
 
 class MBTAReader(object):
-    def __init__(self, key: str=None, return_xml: bool=True):
+    def __init__(self, key: str=None, return_json: bool=True):
         self.headers = {'accept': 'application/vnd.api+json'}
 
         self.key = key
@@ -14,9 +14,9 @@ class MBTAReader(object):
         else:
             print('We\'ll try running without a key! This means that the API will limit us to 20 requests per minute.')
 
-        self.xml = return_xml
-        if self.xml:
-            print('Return type set to raw xml, straight from the MBTA API!')
+        self.json = return_json
+        if self.json:
+            print('Return type set to raw json, straight from the MBTA API!')
         else:
             print('Return type set to simplified objects (all the same data, just easier to get at)!')
 
@@ -27,42 +27,38 @@ class MBTAReader(object):
 
         payload = {}
 
-        # this is a pretty ugly solution to wrapping the variables up for the api
-        # I'm not sure of a better one
+        # this is a pretty ugly solution to wrapping stuff up for the api, sorry!
+        # the fact that arguments switch between filters/fields and filters/names depending on api endpoint is vexing
         for key,value in kwargs.items():
+            if not value: # skip unset arguments
+                continue
+                
             if type(value) == list:
                 value = ",".join(value)
             if type(value) == int:
                 value = str(value)
 
-            if key in ('sort','include') and value:
+            # because I decided to hide whether the names of arguments are passed to the API as filters, fields, or directly (because I think that's an extra level of complexity which isn't actually needed when using the functions), there's an extra step of sorting them into the correct forms while building the payload
+            # ie: 'page[value]', 'fields[value]', or 'filter[value]'
+
+            if key in ('sort','include'):
                 payload[key] = value
-            elif key in ('offset','limit') and value:
+            elif key in ('offset','limit'):
                 payload['page[%s]' % key] = value
-            elif key in ('route_type','direction_id','banner','datetime','lifecycle','severity',
-                'activity','type','latitude','longitude','radius','route_pattern','date',
-                'min_time','max_time','stop_sequence','location_type','name','label') and value:
-                payload['filter[%s]' % key] = value
-            elif key in ('alert','line','prediction','schechedule','shape','vehicle') and value:
+
+            elif key in ('alert','line','prediction','schechedule','shape','vehicle'):
                 payload['fields[%s]' % key] = value
-            elif key == 'id' and value:
-                if '_by_id' in endpoint: payload[key] = value
-                else: payload['filter[%s]' % key] = value
-            elif key == 'route' and value:
-                if 'route' in endpoint: payload['fields[%s]' % key] = value
-                else: payload['filter[%s]' % key] = value
-            elif key == 'stop' and value:
-                if 'stop' in endpoint: payload['fields[%s]' % key] = value
-                else: payload['filter[%s]' % key] = value
-            elif key == 'trip' and value:
-                if 'trip' in endpoint: payload['fields[%s]' % key] = value
-                else: payload['filter[%s]' % key] = value
-            elif key == 'service' and value:
-                if 'service' in endpoint: payload['fields[%s]' % key] = value
-                else: payload['filter[%s]' % key] = value
-            elif key == 'facility' and value:
-                if 'facilit' in endpoint: payload['fields[%s]' % key] = value
-                else: payload['filter[%s]' % key] = value
+
+            elif key == 'id' and '_by_id' in endpoint:
+                payload[key] = value
+
+            elif endpoint in ('routes', 'route_by_id', 'stops', 'stops_by_id',
+                    'trips', 'trip_by_id', 'services', 'service_by_id', 'facilities', 'facility_by_id') \
+                     and key in ('route', 'stop', 'trip', 'service', 'facility'):
+                payload['fields[%s]' % key] = value
+
+            else: # anything not caught in the conditions above should be filters (unless something is badly broken)
+                payload['filter[%s]' % key] = value
         return payload
 
     def __get(self, endpoint: str, payload: dict):
